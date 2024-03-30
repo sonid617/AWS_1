e46a744f23155ca055119707c45388381bde21ed
Abhisht Chouhan <abhishtchouhan@gmail.com>
2024-03-30 09:57:49 +0530

    ADded

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='templates')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# @app.route('/feedback/form')
+# def feedback_form():
+#     return render_template('feedback.html')
+
+# @app.route('/feedback/list')
+# def feedback_list():
+#     return render_template('feedback_list.html')
+
+if __name__ == '__main__':
+    app.run(debug=True,port=5000)
